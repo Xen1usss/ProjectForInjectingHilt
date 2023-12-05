5535d0c38716471c2a8b7e888f9d1486628c75e9
Ksenia <ksyshishe@mail.ru>
2023-12-05 22:07:16 +0300

    included view binding

--- app/src/main/java/app/projectforinjectinghilt/MainActivity.kt
+++ app/src/main/java/app/projectforinjectinghilt/MainActivity.kt
@@ -2,12 +2,19 @@ package app.projectforinjectinghilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.projectforinjectinghilt.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding //байндинг превратил xml файл в класс со своими переменными
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //выделяет под это память
+        val view = binding.root //содержит корневой элемент (констрэинт лэйаут)
+        setContentView(view)
+
     }
 }
