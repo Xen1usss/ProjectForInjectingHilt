9d9bc322592a8b3549e1a2fccb8e676c1c84e963
Ksenia <ksyshishe@mail.ru>
2023-12-23 15:16:38 +0300

    refactoring

--- app/src/main/java/app/projectforinjectinghilt/ui/a/FragmentA.kt
+++ app/src/main/java/app/projectforinjectinghilt/ui/a/FragmentA.kt
@@ -1,4 +1,4 @@
-package app.projectforinjectinghilt.ui
+package app.projectforinjectinghilt.ui.a
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,19 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.fragment.app.viewModels
-import app.projectforinjectinghilt.databinding.FragmentForActivityBinding
+import app.projectforinjectinghilt.databinding.FragmentABinding
+import app.projectforinjectinghilt.ui.a.ViewModelA
 
 @AndroidEntryPoint
-class FragmentForActivityMain : Fragment() {
+class FragmentA : Fragment() {
 
-    private lateinit var binding: FragmentForActivityBinding
-    val viewModel: ViewModelForFragmentMain by viewModels() //нужно ли тут поставить модификатор private?
+    private lateinit var binding: FragmentABinding
+    val viewModel: ViewModelA by viewModels() //нужно ли тут поставить модификатор private?
 
     override fun onCreateView( //?
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentForActivityBinding.inflate(inflater, container, false)
+        binding = FragmentABinding.inflate(inflater, container, false)
         return binding?.root    }
 
     override fun onResume() {
--- app/src/main/java/app/projectforinjectinghilt/ui/a/ViewModelA.kt
+++ app/src/main/java/app/projectforinjectinghilt/ui/a/ViewModelA.kt
@@ -1,11 +1,11 @@
-package app.projectforinjectinghilt.ui
+package app.projectforinjectinghilt.ui.a
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ViewModelForFragmentMain @Inject constructor(val test:String) : ViewModel() {
+class ViewModelA @Inject constructor(val test:String) : ViewModel() {
 
     fun log(){
         println("BBBBBBBBBB $test")
